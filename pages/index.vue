aa24df3a11a91137eed1c96da1f6b6d59cf5a5a1
Yasin Durmuş <ribella.kaneko@gmail.com>
2025-02-11 21:31:22 +0300

    home

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-btn
+      :to="{
+        name: 'auth-login',
+      }"
+      >Giris Yap</v-btn
+    >
+  </v-container>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "auth",
+  middleware: "guest",
+});
+</script>
